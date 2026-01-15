3e75456b7d271191b3727564fca2c6692eec311c
Alfio Puglisi <puglisi_algio@ciao.it>
2026-01-15 09:52:37 +0100

    Inserimento icone python, cobol, DB2 e LangChain nella section stack

--- src/data/techStacks.js
+++ src/data/techStacks.js
@@ -1,11 +1,15 @@
+import cobolLogo from "../assets/stacks/cobol.png";
 import cssLogo from "../assets/stacks/css.png";
+import db2Logo from "../assets/stacks/DB2.png";
 import figmaLogo from "../assets/stacks/figma.png";
 import gitLogo from "../assets/stacks/git.png";
 import htmlLogo from "../assets/stacks/html.png";
 import jsLogo from "../assets/stacks/javascript.png";
+import langchainLogo from "../assets/stacks/langchain.png"
 import mysqlLogo from "../assets/stacks/mysql.png";
 import nodejsLogo from "../assets/stacks/nodejs.png";
 import postgreLogo from "../assets/stacks/postgre.png";
+import pythonLogo from "../assets/stacks/python.png"
 import reactLogo from "../assets/stacks/reactjs.png";
 import reduxLogo from "../assets/stacks/redux.png";
 import tailwindLogo from "../assets/stacks/tailwind.png";
@@ -26,6 +30,19 @@ export const TECH_STACK = [
     src: cssLogo,
     url: "https://www.w3schools.com/css/",
     category: ["frontend"],
+  },
+  {
+    id: "cobol",
+    label: "COBOL",
+    src: cobolLogo,
+    category: ["backend"],
+  },
+  {
+    id: "db2",
+    label: "DB2",
+    src: db2Logo,
+    url: "https://www.ibm.com/support/pages/db2-database-product-documentation",
+    category: ["backend"],
   },
   {
     id: "figma",
@@ -49,6 +66,13 @@ export const TECH_STACK = [
     category: ["frontend"],
   },
   {
+    id: "langchain",
+    label: "LangChain",
+    src: langchainLogo,
+    url: "https://docs.langchain.com/",
+    category: ["backend"],
+  },
+  {
     id: "mysql",
     label: "MySQL",
     src: mysqlLogo,
@@ -67,6 +91,13 @@ export const TECH_STACK = [
     label: "PostgreSQL",
     src: postgreLogo,
     url: "https://www.postgresql.org/",
+    category: ["backend"],
+  },
+  {
+    id: "python",
+    label: "Python",
+    src: pythonLogo,
+    url: "https://www.python.org/",
     category: ["backend"],
   },
   {
